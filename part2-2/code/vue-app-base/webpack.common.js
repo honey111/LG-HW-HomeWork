beb3054ce2265b4be978fb43d06b779a2bc3de73
haowen <1057467537@qq.com>
2020-07-26 23:05:52 +0800

    'part2-2配置文件'

---
+++ part2-2/code/vue-app-base/webpack.common.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'js/bundle.[contenthash:6].js'
+    }
+}
