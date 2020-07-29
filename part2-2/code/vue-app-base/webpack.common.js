f6162284d3eba43806b0023465207db1e226574a
haowen <cuishunsheng@fc18.com.cn>
2020-07-29 20:00:12 +0800

    'webpack配置'

--- part2-2/code/vue-app-base/webpack.common.js
+++ part2-2/code/vue-app-base/webpack.common.js
@@ -1,9 +1,39 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
+    mode: 'none',
     entry: './src/main.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'js/bundle.[contenthash:6].js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.(png|jpg?g|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8 * 1024 //kb
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
