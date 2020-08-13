6813dc1adf9db2151eb350b4a4d0e8fc8b5973d5
haowen <cuishunsheng@fc18.com.cn>
2020-08-13 14:46:18 +0800

    'updata'

---
+++ .history/code3-1/minivue/js/compiler_20200811154414.js
@@ -0,0 +1,12 @@
+class Compiler {
+  constructor (vm) {
+    this.el = vm.$el
+    this.vm = vm
+  }
+  // 编译模板，处理文本节点和元素节点
+  compile (el) {
+
+  }
+  // 编译元素节点，处理指令
+  
+}
---
+++ .history/code3-1/minivue/js/observer_20200811152428.js
@@ -0,0 +1,34 @@
+class Observer {
+  constructor (data){
+    this.walk(data)
+  }
+  walk (data) {
+    // 1. 判断data是否是对象
+    if (!data || typeof data !== 'object') {
+      return
+    }
+    // 2. 遍历data对象的所有属性
+    Object.keys(data).forEach(key => {
+      this,this.defineReactive(data, key, data[key])
+    })
+  }
+  defineReactive (obj, key, val) {
+    // 如果val是对象，把val内部的属性转化为响应式数据
+    this.walk(val)
+    Object.defineProperty(obj, key, {
+      enumerable: true,
+      configurable: true,
+      get () {
+        return val
+      },
+      set (newValue) {
+        if (newValue === val) {
+          return 
+        }
+        val = newValue
+        this.walk(newValue)
+        // 发送通知
+      }
+    })
+  }
+}
