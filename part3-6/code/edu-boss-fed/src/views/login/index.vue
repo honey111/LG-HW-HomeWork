a47aa7adea4b5096b4e87e49eb9b2c6dc70f49dd
hw <1057467537@qq.com>
2021-03-07 23:39:08 +0800

    updata

--- part3-6/code/edu-boss-fed/src/views/login/index.vue
+++ part3-6/code/edu-boss-fed/src/views/login/index.vue
@@ -11,7 +11,7 @@
         <el-input v-model="form.phone"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="form.passworld"></el-input>
+        <el-input v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button class="login-btn" type="primary" @click="onSubmit">登录</el-button>
@@ -22,19 +22,41 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import request from '@/utils/request'
+import qs from 'qs'
+import { Form } from 'element-ui'
 export default Vue.extend({
-  name: 'login',
+  name: 'LoginIndex',
   data () {
     return {
       form: {
-        phone: '',
-        passworld: ''
+        phone: '18201288771',
+        password: '111111'
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    async onSubmit () {
+      // 1、表单验证
+      await (this.$refs.form as Form).validate()
+      // 2、验证通过---提交表单
+      const { data } = await request({
+        method: 'POST',
+        url: '/front/user/login',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(this.form) // axios 默认发送的是application/json 格式的数据
+      })
+      console.log(data)
+      // 3、处理请求结果
+      // 失败：提示
+      if (data.state !== 1) {
+        return this.$message.error(data.message)
+      }
+      // 成功：跳转首页
+      this.$router.push({
+        name: 'home'
+      })
+      this.$message.success('登录成功')
     }
   }
 })
